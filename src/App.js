bb124d4b88cbe0c8d7eae43a3b3c35e06e214c99
zhzhhe <597869582@qq.com>
2020-04-04 18:54:16 +0800

    'cra-root'

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+import Components from './Components'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Components />
+                <h1>Hello!</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
